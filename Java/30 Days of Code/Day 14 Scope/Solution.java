cab0239482408064acab7ade6858f1a00c36afb0
qvizt <qvizt@wtnet.de>
2018-04-27 20:16:43 +0200

    Added solution for Day 14 (30 Days of Code)

---
+++ Java/30 Days of Code/Day 14 Scope/Solution.java
@@ -0,0 +1,48 @@
+package qvizt;
+
+import java.util.Scanner;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    void computeDifference() {
+        int min = elements[0];
+        int max = elements[0];
+        for (int i = 1; i < elements.length; i++) {
+            min = Math.min(min, elements[i]);
+            max = Math.max(max, elements[i]);
+        }
+        maximumDifference = max - min;
+
+        // The short version
+        //        Arrays.sort(elements);
+        //        maximumDifference = elements[elements.length - 1] - elements[0];
+    }
+}
+
+/**
+ * Solution for Day 14: Scope
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
