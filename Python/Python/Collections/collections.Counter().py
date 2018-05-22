1594ba0b0fa828eb740d0caef0354b20f478b4e3
qvizt <>
2018-05-22 16:14:32 +0200

    Added solution collections.Counter()

---
+++ Python/Python/Collections/collections.Counter().py
@@ -0,0 +1,16 @@
+# Solution for collections.Counter()
+from collections import Counter
+
+if __name__ == '__main__':
+    input()  # number of shoes not required
+    shoes = Counter(map(int, input().split()))
+
+    earning = 0
+    customer_nr = int(input())
+    for i in range(customer_nr):
+        size, price = map(int, input().split())
+        if shoes[size]:
+            earning += price
+            shoes[size] -= 1
+
+    print(earning)
