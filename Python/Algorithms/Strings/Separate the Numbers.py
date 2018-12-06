47ffe3606baf396ce3d73de891fbb0aad7076154
qvizt <qvizt@wtnet.de>
2018-12-06 18:07:12 +0100

    Added solution for Separate the Numbers (Algorithms - Strings)

---
+++ Python/Algorithms/Strings/Separate the Numbers.py
@@ -0,0 +1,27 @@
+# Solution for Separate the Numbers
+
+def is_beautiful(s):
+    length = len(s)
+
+    for i in range(0, length // 2):  # split from n to 2 parts
+        first_number_string = s[0:i + 1]
+        nr = int(first_number_string)
+        nr_string = str(nr)
+
+        while len(nr_string) < length:
+            nr += 1
+            nr_string += str(nr)
+
+        if nr_string == s:
+            return 'YES ' + first_number_string
+
+    return 'NO'
+
+
+if __name__ == '__main__':
+    nr = int(input())
+
+    for _ in range(nr):
+        s = input()
+        result = is_beautiful(s)
+        print(result)
