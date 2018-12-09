ed7cc32d1872896251a1a1a3ca0b85fdf0a22b1d
qvizt <qvizt@wtnet.de>
2018-12-09 10:05:10 +0100

    refactoring

--- Python/Algorithms/Sorting/Insertion Sort - Part 1.py
+++ Python/Algorithms/Sorting/Insertion Sort - Part 1.py
@@ -1,20 +1,25 @@
 # Solution for Insertion Sort - Part 1
 
 
+def sort_last_number(numbers):
+    index = len(numbers) - 1
+    nr = numbers[index]
+
+    while index > 0 and nr < numbers[index - 1]:
+        numbers[index] = numbers[index - 1]
+        index -= 1
+        _print(numbers)
+
+    numbers[index] = nr
+    _print(numbers)
+
+
 def _print(numbers):
     print(' '.join(map(str, numbers)))
 
 
 if __name__ == '__main__':
-    index = int(input()) - 1  # using the size as index
+    input()  # skip size
     numbers = list(map(int, input().split()))
 
-    nr = numbers[index]
-    while index > 0 and nr < numbers[index - 1]:
-        numbers[index] = numbers[index - 1]
-        index -= 1
-        _print(numbers)
-    else:
-        numbers[index] = nr
-
-    _print(numbers)
+    sort_last_number(numbers)
