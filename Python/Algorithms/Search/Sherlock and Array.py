636952d561ec1e35201490a9659d3bd525b03bdf
Flo <flo@flo.flo>
2019-04-11 11:25:32 +0200

    Improved solution for Sherlock and Array (Algorithms - Search)

--- Python/Algorithms/Search/Sherlock and Array.py
+++ Python/Algorithms/Search/Sherlock and Array.py
@@ -2,30 +2,28 @@
 
 
 def is_balanced_sum_possible(numbers):
-    if len(numbers) == 1:
-        return 'YES'
-
     left_sum = 0
     right_sum = 0
+    result = 'YES'
 
     # First iteration: different approach than the following iterations
     for i in range(1, len(numbers)):
         right_sum += numbers[i]
 
-    if left_sum == right_sum:
-        return 'YES'
+    if left_sum != right_sum:
+        # Following iterations: Simply add a number to left_sum and
+        # subtract a number of right_sum
+        index = 1
+        while index < len(numbers):
+            left_sum += numbers[index - 1]
+            right_sum -= numbers[index]
+            if left_sum == right_sum:
+                break
+            index += 1
+        else:
+            result = 'NO'
 
-    # Following iterations: Simply add a number to left_sum and
-    # subtract a number of right_sum
-    index = 1
-    while index < len(numbers):
-        left_sum += numbers[index - 1]
-        right_sum -= numbers[index]
-        if left_sum == right_sum:
-            return 'YES'
-        index += 1
-
-    return 'NO'
+    return result
 
 
 if __name__ == '__main__':
